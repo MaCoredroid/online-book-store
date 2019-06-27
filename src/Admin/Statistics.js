8761028d9e588877e80c007733d77bf81322a687
macoredroid <coredroid0401@gmail.com>
2019-06-27 20:01:04 +0800

    add user money

--- src/Admin/Statistics.js
+++ src/Admin/Statistics.js
@@ -17,118 +17,162 @@ class Statistics extends Component {
         super(props);
         this.state = {
             isOpen: false,
-            orders: [
-
-            ],
-            username:Cookies.get("username"),
-            sales:[],
+            orders: [],
+            username: Cookies.get("username"),
+            sales: [],
+            user:[],
             startDate: new Date().setDate(new Date().getDate() - 7),
-            endDate:new Date(),
+            endDate: new Date(),
+            salesisshowing: false
         };
-        this.handlestartdateChange= this.handlestartdateChange.bind(this);
-        this.handleenddateChange= this.handleenddateChange.bind(this);
+        this.handlestartdateChange = this.handlestartdateChange.bind(this);
+        this.handleenddateChange = this.handleenddateChange.bind(this);
     }
-    componentDidMount()
-    {
-        if(Cookies.get("username")==='')
-        {
+
+    componentDidMount() {
+        if (Cookies.get("username") === '') {
             window.location.href = "http://localhost:3000/";
         }
-        let url=Cookies.get('url');
-        axios.get(url+'/admin/seeAllOrder',)
+        let url = Cookies.get('url');
+        axios.get(url + '/admin/seeAllOrder',)
             .then(res => {
                 this.setState(
                     {
                         orders: res.data,
-                        sales:[]
-                    },this.handledateChange);
+                        sales: []
+                    }, this.handledateChange);
             });
     }
 
     toggleCollapse = () => {
-        this.setState({ isOpen: !this.state.isOpen });
+        this.setState({isOpen: !this.state.isOpen});
     };
 
-    handleLogout()
-    {
-        Cookies.set('username','');
+    handleLogout() {
+        Cookies.set('username', '');
         window.location.href = "http://localhost:3000/"
     }
-    handleNavLink(where){
-        window.location.href = "http://localhost:3000/UserManage#/"+ where;
+
+    handleNavLink(where) {
+        window.location.href = "http://localhost:3000/UserManage#/" + where;
     }
-    handledateChange(){
-        let tempbooks = this.state.orders;
-        let res=[];
-        let start=new Date(this.state.startDate).getTime();
-        let end=new Date(this.state.endDate).getTime();
-        for(let i=0;i<tempbooks.length;i++)
-        {
-            let flag=false;
-            if(tempbooks[i].timestamp>=start&&tempbooks[i].timestamp<=end)
-            {
-                for(let j=0;j<res.length;j++)
-                {
-                    if(res[j].bookid===tempbooks[i].bookid)
-                    {
-                        res[j].sales+=tempbooks[i].number;
-                        flag=true;
-                        break;
+
+    handledateChange() {
+        if(this.state.salesisshowing===true) {
+            let tempbooks = this.state.orders;
+            let res = [];
+            let start = new Date(this.state.startDate).getTime();
+            let end = new Date(this.state.endDate).getTime();
+            for (let i = 0; i < tempbooks.length; i++) {
+                let flag = false;
+                if (tempbooks[i].timestamp >= start && tempbooks[i].timestamp <= end) {
+                    for (let j = 0; j < res.length; j++) {
+                        if (res[j].bookid === tempbooks[i].bookid) {
+                            res[j].sales += tempbooks[i].number;
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag === false) {
+                        let book = {
+                            "bookid": tempbooks[i].bookid,
+                            "name": tempbooks[i].name,
+                            "sales": tempbooks[i].number
+                        }
+                        res.push(book);
                     }
                 }
-                if(flag===false)
-                {
-                    let book = {"bookid": tempbooks[i].bookid, "name": tempbooks[i].name, "sales": tempbooks[i].number}
-                    res.push(book);
+            }
+            this.setState({
+                    sales: res
+                }
+            )
+        }
+        else
+        {
+            let tempbooks = this.state.orders;
+            let res = [];
+            let start = new Date(this.state.startDate).getTime();
+            let end = new Date(this.state.endDate).getTime();
+            for (let i = 0; i < tempbooks.length; i++) {
+                let flag = false;
+                if (tempbooks[i].timestamp >= start && tempbooks[i].timestamp <= end) {
+                    for (let j = 0; j < res.length; j++) {
+                        if (res[j].userid === tempbooks[i].userid) {
+                            res[j].money += tempbooks[i].number * tempbooks[i].price/100;
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag === false) {
+                        let book = {
+                            "userid": tempbooks[i].userid,
+                            "name": tempbooks[i].username,
+                            "money": tempbooks[i].number * tempbooks[i].price/100,
+                        }
+                        res.push(book);
+                    }
                 }
             }
+            this.setState({
+                    user: res
+                }
+            )
         }
-        this.setState({
-                sales:res
-            }
-        )
 
     }
-    handlestartdateChange(date){
+
+    handlestartdateChange(date) {
         this.setState({
             startDate: date
         }, this.handledateChange);
     }
-    handleenddateChange(date){
+
+    handleenddateChange(date) {
         this.setState({
             endDate: date
         }, this.handledateChange);
     }
-    handleDate(number)
-    {
-        if(number===1)
-        {
+
+    handleDate(number) {
+        if (number === 1) {
             this.setState({
                 startDate: new Date().setDate(new Date().getDate() - 365),
                 endDate: new Date(),
             }, this.handledateChange);
         }
-        if(number===2)
-        {
+        if (number === 2) {
             this.setState({
                 startDate: new Date().setDate(new Date().getDate() - 30),
                 endDate: new Date(),
             }, this.handledateChange);
         }
-        if(number===3)
-        {
+        if (number === 3) {
             this.setState({
                 startDate: new Date().setDate(new Date().getDate() - 7),
                 endDate: new Date(),
             }, this.handledateChange);
         }
-        if(number===4)
-        {
+        if (number === 4) {
             this.setState({
                 startDate: new Date().setDate(new Date().getDate() - 1),
                 endDate: new Date(),
             }, this.handledateChange);
         }
+    }
+
+    handleUser()
+    {
+        this.setState({
+            salesisshowing: false,
+        },this.handledateChange)
+
+    }
+    handleSales()
+    {
+        this.setState({
+            salesisshowing: true,
+        },this.handledateChange)
     }
     render() {
         return (
@@ -235,12 +279,21 @@ class Statistics extends Component {
                 <p> </p>
                 <MDBTable>
                     <MDBTableHead>
+                        {this.state.salesisshowing?
                         <tr>
                             <th><a>BookID</a></th>
                             <th><a>BookName</a></th>
                             <th><a>Sales</a></th>
                         </tr>
+                            :
+                        <tr>
+                            <th><a>UserID</a></th>
+                            <th><a>UserName</a></th>
+                            <th><a>Total Amount</a></th>
+                        </tr>
+                        }
                     </MDBTableHead>
+                    {this.state.salesisshowing?
                     <MDBTableBody>
                         {this.state.sales.map((item, index) => {
                             return (
@@ -258,12 +311,33 @@ class Statistics extends Component {
                             )
                         })}
                     </MDBTableBody>
+                        :
+                    <MDBTableBody>
+                        {this.state.user.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td >
+                                        {item.userid}
+                                    </td>
+                                    <td >
+                                        {item.name}
+                                    </td>
+                                    <td >
+                                        {item.money}
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </MDBTableBody>
+                    }
                 </MDBTable>
                 <MDBDropdown dropup className="fixed-bottom">
                     <MDBDropdownToggle caret color="primary">
                         Change
                     </MDBDropdownToggle>
                     <MDBDropdownMenu basic>
+                        <MDBDropdownItem onClick={this.handleUser.bind(this)}>Users</MDBDropdownItem>
+                        <MDBDropdownItem onClick={this.handleSales.bind(this)}>Sales</MDBDropdownItem>
                         <MDBDropdownItem onClick={()=>this.handleDate(1)}>In one year</MDBDropdownItem>
                         <MDBDropdownItem onClick={()=>this.handleDate(2)}>In one month</MDBDropdownItem>
                         <MDBDropdownItem onClick={()=>this.handleDate(3)}>In one week</MDBDropdownItem>
